e210c8950514df204b3528a2c739f9eb286a32df
AJ Perez <72290134+albert3rd@users.noreply.github.com>
2022-10-02 20:09:30 -0700

    added subsequence problem

--- arrays.py
+++ arrays.py
@@ -80,3 +80,13 @@ def replace_max_element(arr):
         rightMax = newMax
     return arr
 
+# iS IT A SUBSEQUENCE
+
+def isSubsequence(s,t):
+    i = 0
+    j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i+=1
+        j+=1
+    return i == len(s)
